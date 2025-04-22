dedd41a6abf9e251c667e5c9bc075ef61db058a4
kash munene <158506985+Mp-kash@users.noreply.github.com>
2025-04-22 20:17:34 +0300

    added changes to index.html app.jsx and installed dependancies

--- crypto-place/src/App.jsx
+++ crypto-place/src/App.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Coin from "./pages/Coin/Coin.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase.js";
+import Login from "./pages/Login/Login.jsx";
 
 const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) navigate("/");
+      else navigate("/login");
+    });
+    return () => unsubscribe(); //cleanup
+  }, []);
+
+  // Routes i don't want to have navbar and footer
+  const hideLayout = location.pathname === "/login";
+
   return (
     <div className="app">
-      <Navbar />
+      {!hideLayout && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/coin/:coinId" element={<Coin />} />
       </Routes>
-      <Footer />
+      {!hideLayout && <Footer />}
     </div>
   );
 };
